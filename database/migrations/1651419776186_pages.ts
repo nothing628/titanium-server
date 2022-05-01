b39a3c8fd96f9aad1d5089a63f84e1b8e41800b4
nothing628 <yogyphang@gmail.com>
2022-05-01 22:46:48 +0700

    add page controller

---
+++ database/migrations/1651419776186_pages.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Pages extends BaseSchema {
+  protected tableName = 'pages'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().defaultTo(this.raw('uuid_generate_v4()'))
+      table.uuid('manga_id').notNullable()
+
+      table.integer('page_order').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+      table.foreign('manga_id').references('id').inTable('mangas')
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
