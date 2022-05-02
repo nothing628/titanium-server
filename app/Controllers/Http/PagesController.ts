db0e8c1fd0a024f37965686f2fb035e1bec884fe
nothing628 <yogyphang@gmail.com>
2022-05-02 15:05:19 +0700

    can upload page

--- app/Controllers/Http/PagesController.ts
+++ app/Controllers/Http/PagesController.ts
@@ -1,5 +1,8 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Application from '@ioc:Adonis/Core/Application'
+import CreatePageValidator from 'App/Validators/CreatePageValidator'
 import Manga from 'App/Models/Manga'
+import Page from 'App/Models/Page'
 
 export default class PagesController {
   public async getMangaPage({ params }: HttpContextContract) {
@@ -7,11 +10,36 @@ export default class PagesController {
     const manga = await Manga.query().preload('pages').where('id', mangaId).firstOrFail()
 
     return {
-      data: manga.pages
+      data: manga.pages,
     }
   }
 
-  public async storePage() {}
+  public async storePage({ request, response }: HttpContextContract) {
+    const input = await request.validate(CreatePageValidator)
+    const { file, manga_id, page_order } = input
+    await Manga.findOrFail(manga_id)
+    const fileExts = file.extname
+
+    await file.move(Application.tmpPath('uploads'))
+
+    const pagePath = file.filePath
+    const newPage = await Page.create({
+      mangaId: manga_id,
+      pageOrder: page_order,
+      pagePath: pagePath,
+    })
+
+    response.status(201)
+
+    return {
+      page: {
+        id: newPage.id,
+        manga_id: newPage.mangaId,
+        page_order: newPage.pageOrder,
+        page_path: newPage.pagePath,
+      },
+    }
+  }
 
   public async updatePage() {}
 
--- tests/functional/page_crud.spec.ts
+++ tests/functional/page_crud.spec.ts
@@ -1,5 +1,9 @@
 import { test } from '@japa/runner'
+import Drive from '@ioc:Adonis/Core/Drive'
+import { createReadStream } from 'node:fs'
 import Manga from 'App/Models/Manga'
+import Page from 'App/Models/Page'
+import User from 'App/Models/User'
 
 test.group('Page crud', () => {
   // Write your test here
@@ -26,8 +30,65 @@ test.group('Page crud', () => {
     }
   })
 
-  test('throw 404 when try list page of non-exists manga', async ({client}) => {
+  test('throw 404 when try list page of non-exists manga', async ({ client }) => {
     const response = await client.get('/mangas/3136b941-2f14-493d-9d3c-c070a8784422/pages')
+
+    response.assertStatus(404)
+  })
+
+  test('can upload page', async ({ client, assert }) => {
+    const testManga = await Manga.firstOrFail()
+    const testUser = await User.firstOrFail()
+    const response = await client
+      .post('/pages')
+      .field('manga_id', testManga.id)
+      .field('page_order', 999)
+      .file('file', createReadStream('tmp/testfile.jpg'))
+      .loginAs(testUser)
+
+    response.assertStatus(201)
+
+    const responseBody = response.body()
+
+    // assert.isTrue(isFileUploaded)
+    assert.properties(responseBody, ['page'])
+    assert.properties(responseBody.page, ['id', 'manga_id', 'page_path', 'page_order'])
+
+    const lastPage = await Page.query()
+      .where('page_order', 999)
+      .orderBy('created_at', 'desc')
+      .first()
+
+    assert.isNotNull(lastPage, 'page should be inserted to database')
+    assert.equal(lastPage?.mangaId, testManga.id)
+    assert.propertyVal(responseBody.page, 'id', lastPage?.id)
+    assert.propertyVal(responseBody.page, 'manga_id', lastPage?.mangaId)
+    assert.propertyVal(responseBody.page, 'page_path', lastPage?.pagePath)
+    assert.propertyVal(responseBody.page, 'page_order', lastPage?.pageOrder)
+
+    Drive.restore()
+  })
+
+  test('throw 401 when try upload without access token', async ({ client }) => {
+    const testManga = await Manga.firstOrFail()
+    const response = await client
+      .post('/pages')
+      .field('manga_id', testManga.id)
+      .field('page_order', 999)
+      .file('file', createReadStream('tmp/testfile.jpg'))
+
+    response.assertStatus(401)
+  })
+
+  test('throw 404 when try upload with non-exists manga', async ({ client }) => {
+    const randomUuid = "3136b941-2f14-493d-9d3c-c070a8784422"
+    const testUser = await User.firstOrFail()
+    const response = await client
+      .post('/pages')
+      .field('manga_id', randomUuid)
+      .field('page_order', 999)
+      .file('file', createReadStream('tmp/testfile.jpg'))
+      .loginAs(testUser)
 
     response.assertStatus(404)
   })
