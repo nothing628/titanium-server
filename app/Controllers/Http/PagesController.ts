61cab09a146ce89e82ba639276501cbf1743c4d7
nothing628 <yogyphang@gmail.com>
2022-05-02 13:38:32 +0700

    add test list page

--- app/Controllers/Http/PagesController.ts
+++ app/Controllers/Http/PagesController.ts
@@ -1,8 +1,14 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Manga from 'App/Models/Manga'
 
 export default class PagesController {
   public async getMangaPage({ params }: HttpContextContract) {
     const mangaId = params.id
+    const manga = await Manga.query().preload('pages').where('id', mangaId).firstOrFail()
+
+    return {
+      data: manga.pages
+    }
   }
 
   public async storePage() {}
--- tests/functional/page_crud.spec.ts
+++ tests/functional/page_crud.spec.ts
@@ -1,5 +1,34 @@
 import { test } from '@japa/runner'
+import Manga from 'App/Models/Manga'
 
 test.group('Page crud', () => {
   // Write your test here
+  test('can list pages', async ({ client, assert }) => {
+    const testManga = await Manga.query().preload('pages').firstOrFail()
+    const testPages = testManga.pages
+    const response = await client.get('/mangas/' + testManga.id + '/pages')
+
+    response.assertStatus(200)
+
+    const responseBody = response.body()
+
+    assert.properties(responseBody, ['data'])
+
+    for (const testPage of testPages) {
+      assert.containsSubset(responseBody.data, [
+        {
+          id: testPage.id,
+          manga_id: testManga.id,
+          page_order: testPage.pageOrder,
+          page_path: testPage.pagePath,
+        },
+      ])
+    }
+  })
+
+  test('throw 404 when try list page of non-exists manga', async ({client}) => {
+    const response = await client.get('/mangas/3136b941-2f14-493d-9d3c-c070a8784422/pages')
+
+    response.assertStatus(404)
+  })
 })
